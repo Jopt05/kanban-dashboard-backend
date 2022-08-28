4dd0916dae631e9d9e064c7e361d4fcef8cc6f35
Jesus Puentes <jopt98@gmail.com>
2022-08-27 20:22:12 -0500

    These f permissions are making me sad :(

--- board/views.py
+++ board/views.py
@@ -1,21 +1,39 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
 
 from .serializers import BoardSerializer
 from .models import Board
+from profiles.models import UserProfile
 # Create your views here.
+
+
+class ViewPermissions(BasePermission):
+
+    def has_permissions(self, request, view):
+        return False
 
 
 class BoardsView(APIView):
     """ Boards class """
-    permission_classes = (
-        IsAuthenticated,
-    )
+    permission_classes = [
+        IsAuthenticated | ViewPermissions
+    ]
 
     def get(self, request, user_id):
         """ Returns the boards for the specific user """
+        user_exists = UserProfile.objects.filter(id=user_id).exists()
+
+        if not user_exists:
+            response = {
+                "message": "There is no user with the specified id"
+            }
+            return Response(
+                response,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         queryset = Board.objects.filter(author_id=user_id)
         serializer = BoardSerializer(queryset, many=True)
         response = {
